3d5ea390552feca1b97b633d780a98aa62f15ee0
FrimcX <181182782+FrimcX@users.noreply.github.com>
2025-03-27 16:20:53 -0300

    Add a for loop to create the grid with 16x16 divs and styled them
    Also added a variable named 'containerSize', that gets the shorter value
    between vh or vw to get the container div to be a square.

---
+++ js.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container');
+const containerSize = window.innerWidth > window.innerHeight ? '90vh' : '90vw';
+container.setAttribute('style', `width: ${containerSize}; height: ${containerSize};`);
+//Is it okay? I feel like was hardcode the 90vh or 90vw????
+
+
+let rows = 16;
+let columns = 16;
+
+//Creating the GRID
+for (let i = 1; i <= rows; i++) {
+    for (let j = 0; j < columns; j++) {
+        const pixelDiv = document.createElement('div');
+        pixelDiv.setAttribute('class', 'pixel');
+        pixelDiv.setAttribute('style', `width: ${ (100/ rows) }%; height: ${(100 / columns)}%`);
+        container.appendChild(pixelDiv);
+    }
+}
