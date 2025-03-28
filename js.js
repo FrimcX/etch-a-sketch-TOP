d65245a800a64822ea8690b143f3ce309837def2
FrimcX <181182782+FrimcX@users.noreply.github.com>
2025-03-28 12:25:56 -0300

    Use event delegation and tidy up the code
    In previous versions, I created an eventListener for every square inside
    the container, so in order to use less memory I create only 2 for the
    container. Also deleted unnecesary stuff to make the code more clear.

--- js.js
+++ js.js
@@ -1,11 +1,17 @@
 const container = document.querySelector('.container');
 const containerSize = window.innerWidth > window.innerHeight ? '90vh' : '90vw';
 container.setAttribute('style', `width: ${containerSize}; height: ${containerSize};`);
-//Is it okay? I feel like was hardcode the 90vh or 90vw????
 
-
-let rows = 16;
-let columns = 16;
+function createGrid(rows = 16,columns = 16) {
+    for (let i = 1; i <= rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            const pixelDiv = document.createElement('div');
+            pixelDiv.setAttribute('class', 'pixel');
+            pixelDiv.setAttribute('style', `width: ${ (100/ rows) }%; height: ${(100 / columns)}%`);
+            container.appendChild(pixelDiv);
+        }
+    }
+}
 
 function deleteGrid() {
     const divsToDelete = document.querySelectorAll('.container div');
@@ -26,54 +32,48 @@ button.addEventListener('click', () =>{
             alert('Please input a valid number!');
             break;
         }
-        case (numberUser > 100): {
-            alert('The number needs to be 100 or less');
-            break;
-        } 
-        case (numberUser <= 0): {
-            alert('The number needs to be greater than 0')
+        case ((numberUser > 100) || (numberUser <= 0) ): {
+            alert('The number needs to be greater than 0 and less than 100');
             break;
         }
         default: {
-            rows = numberUser; 
-            columns = rows;
-            console.log('rows: ' + rows);
-            console.log('columns: ' + columns);
             deleteGrid();
-            createGrid(rows,columns);
+            createGrid(numberUser,numberUser);
         }
     }
 })
 
-//Creating the GRID
-function createGrid(rows = 16,columns = 16) {
-    for (let i = 1; i <= rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            const pixelDiv = document.createElement('div');
-            pixelDiv.setAttribute('class', 'pixel');
-            pixelDiv.setAttribute('style', `width: ${ (100/ rows) }%; height: ${(100 / columns)}%`);
-            container.appendChild(pixelDiv);
-            pixelDiv.addEventListener('mouseenter', ()=> {
-                let pixelDivClass = pixelDiv.getAttribute('class');
-                if ((pixelDivClass !== 'pixel mouse-enter') && 
-                    (pixelDivClass !== 'pixel mouse-enter mouse-leave')) {
-                        pixelDiv.setAttribute('class', (pixelDivClass + ' mouse-enter'));
-                }
-            });
-            pixelDiv.addEventListener('mouseleave', ()=> {
-                let pixelDivClass = pixelDiv.getAttribute('class');
-                if (pixelDivClass !== 'pixel mouse-enter mouse-leave') {
-                    pixelDiv.setAttribute('class', (pixelDivClass + ' mouse-leave'));
-                    pixelDiv.style.backgroundColor = `rgb(${randomClr()},${randomClr()},${randomClr()})`;
-                    pixelDiv.style.opacity = '1';
-                }
-                if (pixelDivClass === 'pixel mouse-enter mouse-leave') {
-                    let opacity = pixelDiv.style.opacity;
-                    pixelDiv.style.opacity = `${opacity - 0.1}`; 
-                }
-            });
+container.addEventListener('mouseover', (e)=> {
+    let pixelDivClass = e.target.classList;
+
+    if (!e.target.classList.contains('container')) {
+
+        if (!e.target.classList.contains('mouse-enter')
+            && !e.target.classList.contains('mouse-leave')) {
+
+                e.target.setAttribute('class', (pixelDivClass + ' mouse-enter'));
+
         }
     }
-}
+});
+
+container.addEventListener('mouseout', (e)=> {
+    let pixelDivClass = e.target.classList;
+    
+    if (!e.target.classList.contains('container')) {
+        
+        if (!e.target.classList.contains('mouse-leave')) {
+            e.target.setAttribute('class', (pixelDivClass + ' mouse-leave'));
+            e.target.style.backgroundColor = `rgb(${randomClr()},${randomClr()},${randomClr()})`;
+            e.target.style.opacity = '1';
+        }
+        else if (e.target.classList.contains('mouse-leave') &&
+            e.target.classList.contains('pixel')) {
+                
+            e.target.style.opacity = `${e.target.style.opacity - 0.1}`; 
+        }
+    }
+});
+
 
 createGrid();
