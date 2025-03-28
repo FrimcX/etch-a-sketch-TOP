e42482dc2dc7d0f56fd028b0d20440c8eba82fb6
FrimcX <181182782+FrimcX@users.noreply.github.com>
2025-03-27 22:55:58 -0300

    Add a button to change the number of squares in the etch-a-sketch
    To get the desired result, I moved the main for loop that created the
    squares inside a function. Also created a function to delete previous
    squares, and the button activates those functions via an eventListener.

--- js.js
+++ js.js
@@ -7,20 +7,58 @@ container.setAttribute('style', `width: ${containerSize}; height: ${containerSiz
 let rows = 16;
 let columns = 16;
 
-//Creating the GRID
-for (let i = 1; i <= rows; i++) {
-    for (let j = 0; j < columns; j++) {
-        const pixelDiv = document.createElement('div');
-        pixelDiv.setAttribute('class', 'pixel');
-        pixelDiv.setAttribute('style', `width: ${ (100/ rows) }%; height: ${(100 / columns)}%`);
-        container.appendChild(pixelDiv);
-        pixelDiv.addEventListener('mouseenter', ()=> {
-            let pixelDivClass = pixelDiv.getAttribute('class');
-            pixelDiv.setAttribute('class', (pixelDivClass + ' mouse-enter'));
-        });
-        pixelDiv.addEventListener('mouseleave', ()=> {
-            let pixelDivClass = pixelDiv.getAttribute('class');
-            pixelDiv.setAttribute('class', (pixelDivClass + ' mouse-leave'));
-        });
+function deleteGrid() {
+    const divsToDelete = document.querySelectorAll('.container div');
+    for (let i = 0; i < divsToDelete.length; i++) {
+        divsToDelete[i].remove();
     }
 }
+
+const button = document.querySelector('#button-number');
+button.addEventListener('click', () =>{
+    let numberUser = Number(prompt('How many rows/columns you want?'));
+    switch (true) {
+        case (numberUser == ''): {
+            alert('Please input a valid number!');
+            break;
+        }
+        case (numberUser > 100): {
+            alert('The number needs to be 100 or less');
+            break;
+        } 
+        case (numberUser <= 0): {
+            alert('The number needs to be greater than 0')
+            break;
+        }
+        default: {
+            rows = numberUser; 
+            columns = rows;
+            console.log('rows: ' + rows);
+            console.log('columns: ' + columns);
+            deleteGrid();
+            createGrid(rows,columns);
+        }
+    }
+})
+
+//Creating the GRID
+function createGrid(rows = 16,columns = 16) {
+    for (let i = 1; i <= rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            const pixelDiv = document.createElement('div');
+            pixelDiv.setAttribute('class', 'pixel');
+            pixelDiv.setAttribute('style', `width: ${ (100/ rows) }%; height: ${(100 / columns)}%`);
+            container.appendChild(pixelDiv);
+            pixelDiv.addEventListener('mouseenter', ()=> {
+                let pixelDivClass = pixelDiv.getAttribute('class');
+                pixelDiv.setAttribute('class', (pixelDivClass + ' mouse-enter'));
+            });
+            pixelDiv.addEventListener('mouseleave', ()=> {
+                let pixelDivClass = pixelDiv.getAttribute('class');
+                pixelDiv.setAttribute('class', (pixelDivClass + ' mouse-leave'));
+            });
+        }
+    }
+}
+
+createGrid();
